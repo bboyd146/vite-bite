cff42e76e9c54c00343ce96459e6cbaebf64b4fb
bboyd146 <82745040+bboyd146@users.noreply.github.com>
2023-07-30 00:27:49 -0500

    server setup

---
+++ server/server.js
@@ -0,0 +1,22 @@
+const express = require('express')
+const path = require('path')
+
+const PORT = process.env.PORT || 3001;
+const app = express()
+
+app.use(express.urlencoded({ extended: false }));
+app.use(express.json());
+
+
+
+if (process.env.NODE_ENV === 'production') {
+    app.use(express.static(path.join(__dirname, '../client/dist')));
+
+    app.get('*', (req, res) => {
+        res.sendFile(path.join(__dirname, '../client/dist/index.html'));
+    });
+}
+
+    app.listen(PORT, () => {
+        console.log(`API server running on port ${PORT}!`);
+    });

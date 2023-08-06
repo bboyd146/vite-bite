cda78c14efeeb8b8b3372e005c43d206d2b4f60a
bboyd146 <82745040+bboyd146@users.noreply.github.com>
2023-08-06 00:52:31 -0500

    added model to handle followers

---
+++ server/models/NewFollow.js
@@ -0,0 +1,27 @@
+const { mongoose, Schema } = require('mongoose');
+
+const NewFollowSchema = new Schema ({
+    sender: {
+        type: Schema.Types.ObjectId,
+        ref: 'User',
+        required: true
+    },
+    receiver: {
+        type: Schema.Types.ObjectId,
+        ref: 'User',
+        required: true
+    },
+    status: {
+        type: String,
+        enum: ['pending', 'accepted', 'declined'],
+        default: 'pending'
+    },
+    createdAt:{
+        type: Date,
+        default: Date.now
+    }
+})
+
+const NewFollow = mongoose.model('NewFollow', NewFollowSchema)
+
+module.exports = NewFollow

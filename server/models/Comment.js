66f80137e9b0197a8f255c523d8d1f6474ac57d2
bboyd146 <82745040+bboyd146@users.noreply.github.com>
2023-08-06 00:37:43 -0500

    made comment model

---
+++ server/models/Comment.js
@@ -0,0 +1,26 @@
+const { mongoose, Schema } = require('mongoose');
+
+const commentSchema = new Schema ({
+    content: {
+        type: String,
+        required: true,
+        maxLength: 350,
+    },
+    author: {
+        type: Schema.Types.ObjectId,
+        ref: 'User',
+        required: true,
+    },
+    post: {
+        type: Schema.Types.ObjectId,
+        ref: 'Post'
+    },
+    createdAt: {
+        type: Date,
+        default: Date.now
+    }
+})
+
+const Comment = mongoose.model('Comment', commentSchema)
+
+module.exports = Comment

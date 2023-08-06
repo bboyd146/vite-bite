2a2ba0624b36c94c334f05e7440252c0cfd45fbc
bboyd146 <82745040+bboyd146@users.noreply.github.com>
2023-08-06 00:40:05 -0500

    added password hashing and compare to user model

--- server/models/User.js
+++ server/models/User.js
@@ -1,4 +1,5 @@
 const { mongoose, Schema } = require('mongoose');
+const bcrypt = require('bcrypt');
 
 const userSchema = new Schema({
     username: {
@@ -33,6 +34,19 @@ const userSchema = new Schema({
     }],
 },
     { timestamps: true });
+
+userSchema.pre('save', async function (next) {
+    if (this.isNew || this.isModified('password')) {
+        const saltRounds = 10;
+        this.password = await bcrypt.hash(this.password, saltRounds);
+    }
+
+    next();
+});
+
+userSchema.methods.isCorrectPassword = async function (password) {
+    return await bcrypt.compare(password, this.password);
+};
 
 const User = mongoose.model('User', userSchema);
 

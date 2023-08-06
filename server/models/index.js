f1872bd97e97059f86c2ccc4eec80628d474510a
bboyd146 <82745040+bboyd146@users.noreply.github.com>
2023-08-06 00:57:20 -0500

    index configured

---
+++ server/models/index.js
@@ -0,0 +1,6 @@
+const User = require('./User')
+const Post = require('./Post')
+const Comment = require('./Comment')
+const NewFollow = require('./NewFollow')
+
+module.exports = { User, Post, Comment, NewFollow}

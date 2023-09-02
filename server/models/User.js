6ab2a57dc321695aab4ee655842f16df7fbb1104
bboyd146 <82745040+bboyd146@users.noreply.github.com>
2023-09-02 14:29:26 -0500

    added posts to user model

--- server/models/User.js
+++ server/models/User.js
@@ -33,6 +33,10 @@ const userSchema = new Schema({
         type: Schema.Types.ObjectId,
         ref: 'User'
     }],
+    posts: [{
+        type: Schema.Types.ObjectId,
+        ref: 'Post'
+    }]
 },
     { timestamps: true });
 

0999a84e192f93bc600312264e2c8fb3407993a5
bboyd146 <82745040+bboyd146@users.noreply.github.com>
2023-09-02 01:13:59 -0500

    typedefs work

---
+++ server/schemas/typeDefs.js
@@ -0,0 +1,37 @@
+const { gql } = require('apollo-server-express');
+
+const typeDefs = gql `
+    type User {
+        _id: ID
+        username: String
+        email: String
+        bio: String
+        followers: [User]
+        following: [User]
+    }
+    type Comment {
+        _id: ID
+        content: String
+        author: User
+        post: Post
+        likes: [User]
+        initialComment: Comment
+        createdAt: String
+    }
+    type NewFollow {
+        _id: ID
+        sender: User
+        receiver: User
+        status: String
+        createdAt: String
+    }
+    type Post {
+        _id: ID
+        content: String
+        author: User
+        likes: [User]
+        re_post: [User]
+    }
+`
+
+module.exports = typeDefs

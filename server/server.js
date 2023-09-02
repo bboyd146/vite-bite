e45b31da1d49ebf411a4b53804041831d1bea779
bboyd146 <82745040+bboyd146@users.noreply.github.com>
2023-09-02 14:46:02 -0500

    added ApolloServer

--- server/server.js
+++ server/server.js
@@ -1,10 +1,17 @@
 const express = require('express')
 const path = require('path')
 const { ApolloServer } = require('apollo-server-express');
-const PORT = process.env.PORT || 3001;
 const app = express()
-const { typeDefs, resolvers } = require('./schemas');
 const db = require('./config/connection');
+const { typeDefs, resolvers } = require('./schemas');
+const { authMiddleware } = require('./utils/auth');
+const PORT = process.env.PORT || 3001;
+
+const server = new ApolloServer({
+    typeDefs,
+    resolvers,
+    context: authMiddleware,
+});
 
 app.use(express.urlencoded({ extended: false }));
 app.use(express.json());
@@ -12,13 +19,24 @@ app.use(express.json());
 
 
 if (process.env.NODE_ENV === 'production') {
-    app.use(express.static(path.join(__dirname, '../client/dist')));
+    app.use(express.static(path.join(__dirname, '../client/build')));
 
     app.get('*', (req, res) => {
-        res.sendFile(path.join(__dirname, '../client/dist/index.html'));
+        res.sendFile(path.join(__dirname, '../client/build/index.html'));
     });
 }
 
-    app.listen(PORT, () => {
-        console.log(`API server running on port ${PORT}!`);
-    });
+const startApolloServer = async () => {
+    await server.start();
+    server.applyMiddleware({ app });
+
+    db.once('open', () => {
+        app.listen(PORT, () => {
+            console.log(`API server running on port ${PORT}!`);
+            console.log(`Use GraphQL at http://localhost:${PORT}${server.graphqlPath}`);
+        })
+    })
+};
+
+startApolloServer();
+
